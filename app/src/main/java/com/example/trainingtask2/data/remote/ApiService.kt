55f52960f14234fab844495f6d32a3f86301db07
Astrid Coenrad <astridcoenradina@gmail.com>
2021-11-06 21:14:15 +0700

    Enhance UI & Fix bug on API Call

--- app/src/main/java/com/example/trainingtask2/data/remote/ApiService.kt
+++ app/src/main/java/com/example/trainingtask2/data/remote/ApiService.kt
@@ -1,11 +1,10 @@
 package com.example.trainingtask2.data.remote
 
 import com.example.trainingtask2.data.model.CartoonModel
-import com.example.trainingtask2.data.model.CartoonResultModel
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
     @GET("character")
-    suspend fun fetchCartoonById(@Query("page_id") page_id : Int): CartoonModel
+    suspend fun fetchCartoonById(@Query("page") page_id : Int): CartoonModel
 }
